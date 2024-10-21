f003220d08668999344781b507a05d0bb2c22185
michael.zabolocki <mzabolocki@gmail.com>
2024-10-21 14:35:11 +0200

    checks for acg plots

--- synapticonn/plots/__init__.py
+++ synapticonn/plots/__init__.py
@@ -1,3 +1,4 @@
 """Sub-module for plot functions."""
 
 from .acg import plot_acg
+from .checks import check_ndim, check_empty
--- synapticonn/plots/acg.py
+++ synapticonn/plots/acg.py
@@ -4,27 +4,43 @@ acg.py
 Modules for plotting autocorrelograms.
 """
 
+import pathlib
+
 import numpy as np
 import matplotlib.pyplot as plt
 
+from matplotlib import style
+
 from synapticonn.utils.mod_utils import check_dependency
+from synapticonn.plots.checks import check_empty, check_ndim
+
+
+##########################################################
+##########################################################
+
+
+style.use(pathlib.Path('synapticonn', 'plots', 'plots.mplstyle'))
 
 
 ##########################################################
 ##########################################################
 
 @check_dependency(plt, 'matplotlib')
-def plot_acg(acg, bin_size, t_stop, ax=None, **kwargs):
-    """Plot an autocorrelogram.
+@check_ndim
+@check_empty
+def plot_acg(spike_train_ms, bin_size_ms=1, max_lag_ms=100, ax=None, **kwargs):
+    """Plot an autocorrelogram for a single spike train.
 
     Parameters
     ----------
-    acg : array-like
-        Autocorrelogram to plot.
-    bin_size : float
-        Bin size of the autocorrelogram.
-    t_stop : float
-        Maximum time to plot.
+    spike_train_ms : array-like
+        Spike times (in milliseconds).
+    bin_size_ms : float
+        Bin size of the autocorrelogram (in milliseconds).
+        Default is 1 ms.
+    max_lag_ms : float
+        Maximum lag to compute the autocorrelogram (in milliseconds).
+        Default is 100 ms.
     ax : matplotlib.axes.Axes, optional
         Axis to plot on.
     **kwargs
@@ -38,22 +54,22 @@ def plot_acg(acg, bin_size, t_stop, ax=None, **kwargs):
     if ax is None:
         ax = plt.gca()
 
-    time = np.arange(-t_stop, t_stop + bin_size, bin_size)
-    ax.bar(time, acg, width=bin_size, **kwargs)
+    lags, autocorr = _compute_autocorrelogram(spike_train_ms, bin_size_ms, max_lag_ms)
 
-    ax.set_xlabel('Time (ms)')
-    ax.set_ylabel('Count')
+    ax.bar(lags, autocorr, width=bin_size_ms, **kwargs)
+    ax.set_xlabel('Time lag (ms)')
+    ax.set_ylabel('Spike counts/bin')
 
     return ax
 
 
-def _compute_autocorrelogram(spike_train, bin_size_ms=1, max_lag_ms=100):
+def _compute_autocorrelogram(spike_train_ms, bin_size_ms=1, max_lag_ms=100):
     """ Compute the autocorrelogram of a spike train.
 
     Parameters
     ----------
-    spike_train : array-like
-        Spike times (in seconds).
+    spike_train_ms : array-like
+        Spike times (in milliseconds).
     bin_size_ms : float, optional
         Bin size of the autocorrelogram (in milliseconds).
     max_lag_ms : float, optional
@@ -67,23 +83,21 @@ def _compute_autocorrelogram(spike_train, bin_size_ms=1, max_lag_ms=100):
         Autocorrelogram values.
     """
 
-    # convert max_lag and bin_size to bins
-    max_lag_bins = int(max_lag_ms / bin_size_ms)
+    # compute the differences between all spike times (in ms)
+    spike_diffs = np.subtract.outer(spike_train_ms, spike_train_ms)
 
-    # compute the differences between all spike times
-    spike_diffs = np.subtract.outer(spike_train, spike_train)
-
-    # only keep differences within the maximum lag
+    # keep only differences within the maximum lag
     spike_diffs = spike_diffs[np.abs(spike_diffs) <= max_lag_ms]
 
-    # compute histogram (binning the differences)
-    autocorr, bin_edges = np.histogram(spike_diffs, bins=np.arange(-max_lag_ms, max_lag_ms + bin_size_ms, bin_size_ms))
+    # compute histogram (binning the differences with given bin size)
+    bins = np.arange(-max_lag_ms, max_lag_ms + bin_size_ms, bin_size_ms)
+    autocorr, bin_edges = np.histogram(spike_diffs, bins=bins)
 
     # remove the zero-lag bin (since it's the same spike)
-    zero_bin = int(len(autocorr) // 2)
+    zero_bin = len(autocorr) // 2
     autocorr[zero_bin] = 0
 
-    # compute lag values
+    # compute lag values centered on each bin
     lags = bin_edges[:-1] + bin_size_ms / 2
 
     return lags, autocorr
