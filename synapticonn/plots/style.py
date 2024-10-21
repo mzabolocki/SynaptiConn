01bac5eccd153f13b35bc15a32231445b9632cf0
michael.zabolocki <mzabolocki@gmail.com>
2024-10-21 13:34:45 +0200

    use plot styling

--- synapticonn/plots/style.py
+++ synapticonn/plots/style.py
@@ -1,4 +1,10 @@
 """Style and aesthetics definitions for plots."""
 
+import pathlib
+
 from itertools import cycle
 from functools import wraps
+
+from matplotlib.pyplot import style
+
+style.use(pathlib.Path('synapticonn', 'plots', 'plots.mplstyle'))
