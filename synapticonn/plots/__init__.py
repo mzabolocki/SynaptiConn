4ad236cdd55e2f682234a54fa545973520030b9d
michael.zabolocki <mzabolocki@gmail.com>
2024-10-23 17:16:54 +0200

    update paths

--- synapticonn/plots/__init__.py
+++ synapticonn/plots/__init__.py
@@ -1,6 +1,8 @@
 """Sub-module for plot functions."""
 
 from .acg import plot_acg
+from .ccg import plot_ccg
 
-from .checks import check_ndim, check_empty, check_millisecond
+from .spiketrain_utils import check_spiketrain_ndim, check_spiketrain_millisecond
+from .plot_utils import check_ax
 from .style import apply_plot_style
