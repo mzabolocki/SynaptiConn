6e8b4faa9df76d3ab38314f2c5354f7045b8b28b
michael.zabolocki <mzabolocki@gmail.com>
2024-11-15 17:12:40 +0100

    setup update

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup, find_packages
 
+with open('README.rst') as readme_file:
+    long_description = readme_file.read()
+
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
     name="synapticonn",
     version="0.0.1",
-    description="Inferring monosynaptic connections in neural circuits",
+    description="Inferring monosynaptic connections in neural circuits.",
     author="Michael Zabolocki",
+    author_email='mzabolocki@gmail.com',
+    maintainer_email='mzabolocki@gmail.com',
+    url='https://github.com/mzabolocki/SynaptiConn',
+    long_description=long_description,
     packages=find_packages(),
     install_requires=requirements,
 )
