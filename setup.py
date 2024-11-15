aa324898a2543dbbda319a4f4d1ac36aa0fea76e
michael.zabolocki <mzabolocki@gmail.com>
2024-11-15 17:04:45 +0100

    install requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="synapticonn",
     version="0.0.1",
+    description="Inferring monosynaptic connections in neural circuits",
+    author="Michael Zabolocki",
     packages=find_packages(),
+    install_requires=requirements,
 )
