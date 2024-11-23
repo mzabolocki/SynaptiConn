a6773f051d1b6540959279234135f4952a3d7057
michael.zabolocki <mzabolocki@gmail.com>
2024-11-23 22:06:06 +0100

    err check for spk times within time lag

--- synapticonn/postprocessing/autocorrelograms.py
+++ synapticonn/postprocessing/autocorrelograms.py
@@ -11,31 +11,35 @@ from synapticonn.postprocessing.correlogram_utils import make_bins
 ##########################################################
 
 
-def compute_autocorrelogram(spike_train_ms, bin_size_t=1, max_lag_t=100):
+def compute_autocorrelogram(spike_train, bin_size_t=1, max_lag_t=100):
     """ Compute the autocorrelogram of a spike train.
 
     Parameters
     ----------
-    spike_train_ms : array-like
-        Spike times (in milliseconds).
+    spike_train : array-like
+        Spike times.
     bin_size_t : float, optional
-        Bin size of the autocorrelogram (in milliseconds).
+        Bin size of the autocorrelogram.
     max_lag_t : float, optional
-        Maximum lag to compute the autocorrelogram (in milliseconds).
+        Maximum lag to compute the autocorrelogram.
 
     Returns
     -------
     lags : array-like
-        Lag values (in milliseconds).
+        Lag values.
     autocorr : array-like
         Autocorrelogram values.
     """
 
-    # compute the differences between all spike times (in ms)
-    spike_diffs = np.subtract.outer(spike_train_ms, spike_train_ms)
+    # compute the differences between all spike times
+    spike_diffs = np.subtract.outer(spike_train, spike_train)
 
     # keep only differences within the maximum lag
     spike_diffs = spike_diffs[np.abs(spike_diffs) <= max_lag_t]
+
+    if len(spike_diffs) == 0:
+        raise ValueError("No spikes within the maximum lag time."
+                         " Please check the spike times and the maximum lag time.")
 
     # compute histogram (binning the differences with given bin size)
     bins = make_bins(max_lag_t, bin_size_t)
