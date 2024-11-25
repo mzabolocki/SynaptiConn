54b7944e996602e4b1abc366b5d4055bd08c6bf5
michael.zabolocki <mzabolocki@gmail.com>
2024-11-25 10:48:33 +0100

    version="0.0.1rc1"

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def parse_requirements(filename):
 if __name__ == "__main__":
     setup(
         name="synapticonn",
-        version="0.0.1",
+        version="0.0.1rc1",
         description="Inferring monosynaptic connections in neural circuits.",
         long_description=open("README.rst", "r", encoding="utf-8").read(),
         long_description_content_type="text/x-rst",
--- synapticonn/version.py
+++ synapticonn/version.py
@@ -1,3 +1,3 @@
 """ set the current pypi version. """
 
-__version__ = '0.0.1'
+__version__ = '0.0.1rc1'
