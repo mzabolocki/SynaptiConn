e74584fd2a293682425945341651e615bd3840b5
michael.zabolocki <mzabolocki@gmail.com>
2024-11-25 10:21:02 +0100

    remove reduncies

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    long_description = readme_file.read()
+def parse_requirements(filename):
+    """Read dependencies from a requirements file."""
+    with open(filename, "r", encoding="utf-8") as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
-setup(
-    name="synapticonn",
-    version="0.0.1",
-    description="Inferring monosynaptic connections in neural circuits.",
-    author="Michael Zabolocki",
-    author_email='mzabolocki@gmail.com',
-    maintainer_email='mzabolocki@gmail.com',
-    url='https://github.com/mzabolocki/SynaptiConn',
-    long_description=long_description,
-    packages=find_packages(),
-    install_requires=requirements,
-)
+if __name__ == "__main__":
+    setup(
+        name="synapticonn",
+        version="0.0.1",
+        description="Inferring monosynaptic connections in neural circuits.",
+        long_description=open("README.rst", "r", encoding="utf-8").read(),
+        long_description_content_type="text/x-rst",
+        author="Michael Zabolocki",
+        author_email="mzabolocki@gmail.com",
+        maintainer_email="mzabolocki@gmail.com",
+        url="https://github.com/mzabolocki/SynaptiConn",
+        packages=find_packages(),
+        install_requires=parse_requirements("requirements.txt"),
+        extras_require={
+            "testing": parse_requirements("requirements_dev.txt"),
+        },
+        python_requires=">=3.8",
+        include_package_data=True,
+        zip_safe=False,
+    )
