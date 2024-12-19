ef37cd90b74aac2b89d6a329c167e56fd2cfd1df
Michael Zabolocki <89680425+mzabolocki@users.noreply.github.com>
2024-12-19 17:17:43 +1030

    debug latency
    Error. Was not computing correctly. / 2 to accunt for time difference between bin centers.

--- synapticonn/features/mono_connection_latency.py
+++ synapticonn/features/mono_connection_latency.py
@@ -19,17 +19,24 @@ def compute_peak_latency(ccg, bin_size_t):
         Cross-correlogram.
     bin_size_t : float
         Bin size of the cross-correlogram.
+        This is the time difference between bins, 
+        not there centres.
 
     Returns
     -------
     output : float
         Timing of the peak of the CCG
+        
+    Notes
+    -----
+    The bin size is the time difference between bins, not the bin width.
+    Here, we calculate the time difference between the peak and the center of the CCG.
     """
 
     # find the peak of the cross-correlogram
     peak = np.argmax(ccg)
 
     # convert the peak to time
-    peak_time = (peak - len(ccg) // 2) * bin_size_t
+    peak_time = (peak - len(ccg) // 2) * (bin_size_t / 2) # time difference between bin centers
 
     return {'ccg_peak_time_ms': peak_time}
